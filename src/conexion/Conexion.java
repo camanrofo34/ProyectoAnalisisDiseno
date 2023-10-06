72b53aa78fc607c23d6fb9e22d9047ea33f9b957
CaRf <camanrofo@gmail.com>
2023-10-06 17:11:02 -0500

    "Creación proyecto"

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,24 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ediso
+ */
+public class Conexion {
+
+    //conexion local
+    public static Connection conectar() {
+        try {
+            String url = "jdbc:mysql://" + "localhost" + ":" + 3306 + "/" + "bd_sistema_ventas" + "?user=" + "root" + "&password=" + "1097096174" + "&useSSL=false";
+            Connection cn = DriverManager.getConnection(url);
+            return cn;
+        } catch (SQLException e) {
+            System.out.println("Error en la conexion local " + e);
+        }
+        return null;
+    }
+}
